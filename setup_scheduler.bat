9256b2ff242ee56b6242f6654d8d5a2683b9cac2
DRAGONDINESH <lingamdinesh6@gmail.com>
2025-10-28 23:49:31 +0530

    Report is Generating

---
+++ setup_scheduler.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Setting up Windows Task Scheduler for AutoPatch...
+
+:: Create scheduled task to run daily at 2 AM
+schtasks /create /tn "AutoPatch-Updater" /tr "D:\projects\srinath\AutoPatch\autopatch.bat" /sc daily /st 02:00 /ru SYSTEM
+
+echo.
+echo Scheduled task created!
+echo Task Name: AutoPatch-Updater
+echo Schedule: Daily at 2:00 AM
+echo.
+echo To view the task: schtasks /query /tn "AutoPatch-Updater"
+echo To delete the task: schtasks /delete /tn "AutoPatch-Updater"
+pause
